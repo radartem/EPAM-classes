14d63cafb7de2dfbbdf1f3556bc57bc9d6c528c8
Artem Radchikov <artem.radchikov39@gmail.com>
2019-10-22 16:02:36 +0300

    Triangle inequality UnitTests renew

--- Triangle/TriangleUnitTests/TriangleUntTests.cs
+++ Triangle/TriangleUnitTests/TriangleUntTests.cs
@@ -8,91 +8,91 @@ namespace TriangleUnitTests
     {
         
         [TestMethod]
-        public void CheckTriangleInequality_FirstSideMoreSumOtherTwo_false()
+        public void  TriangleInequality_FirstSideMoreSumOtherTwo_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(10, 5,4 ));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_SecondSideMoreSumOtherTwo_false()
+        public void TriangleInequality_SecondSideMoreSumOtherTwo_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(4, 10, 5));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_ThirdSideMoreSumOtherTwo_false()
+        public void TriangleInequality_ThirdSideMoreSumOtherTwo_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(4, 5, 10));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_NegativeFirstSide_false()
+        public void TriangleInequality_NegativeFirstSide_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(-10, 5, 4));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_NegativeSecondSide_false()
+        public void TriangleInequality_NegativeSecondSide_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(4, -10, 5));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_NegativeThirdSide_false()
+        public void TriangleInequality_NegativeThirdSide_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(4, 5, -10));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_NegativeAllSides_false()
+        public void TriangleInequality_NegativeAllSides_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(-9, -5, -7));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_ZeroFirstSide_false()
+        public void TriangleInequality_ZeroFirstSide_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(0, 5, 4));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_ZeroSecondSide_false()
+        public void TriangleInequality_ZeroSecondSide_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(4, 0, 5));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_ZeroThirdSide_false()
+        public void TriangleInequality_ZeroThirdSide_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(4, 5, 0));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_ZeroAllSides_false()
+        public void TriangleInequality_ZeroAllSides_false()
         {
             Assert.IsFalse(Triangle.Triangle.CheckTriangleInequality(0, 0, 0));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_EquilateralTriangle_true()
+        public void TriangleInequality_EquilateralTriangle_true()
         {
             Assert.IsTrue(Triangle.Triangle.CheckTriangleInequality(10, 10, 10));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_IsoscelesTriangle_true()
+        public void TriangleInequality_IsoscelesTriangle_true()
         {
             Assert.IsTrue(Triangle.Triangle.CheckTriangleInequality(10, 10, 9));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_RectangularTriangle_true()
+        public void TriangleInequality_RectangularTriangle_true()
         {
             Assert.IsTrue(Triangle.Triangle.CheckTriangleInequality(6, 8, 10));
         }
 
         [TestMethod]
-        public void CheckTriangleInequality_CommonTriangle_true()
+        public void TriangleInequality_CommonTriangle_true()
         {
             Assert.IsTrue(Triangle.Triangle.CheckTriangleInequality(6, 5, 9));
         }
